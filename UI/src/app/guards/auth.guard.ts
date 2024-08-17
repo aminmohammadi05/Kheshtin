6bb94f4800e2c2ce3c3846dab73d9cdf28be20a4
Amin Mohammadi <amin_mohammadi05@yahoo.com>
2024-08-17 15:26:15 +0330

    Upgrade to version 16

--- UI/src/app/guards/auth.guard.ts
+++ UI/src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { AlertifyService } from '../services/alertify.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -9,7 +9,7 @@ import { Settings, AppSettings } from '../app.settings';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   public settings: Settings;
   constructor(private authService: AuthService,
               private router: Router,
